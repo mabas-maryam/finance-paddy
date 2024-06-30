0343fb5deff71a59a57555d231dbd5c36d5e25ed
unknown <mabas.maryam>
2024-06-30 10:32:26 +0100

    worked on Navbar, Using React Router to link the components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
+import React from "react"
+import {Routes, Route} from 'react-router-dom'
+
 import LandingPage from "./LandingPage"
+import Hero from "./components/landingPage/Hero"
+import SignIn from "./components/sign/SignIn"
+import SignUp from "./components/sign/SignUp"
+import Developers from "./components/Developers"
 
 function App() {
   return (
     <>
-      <LandingPage />
+      <Routes>
+       <Route path="/" element={<LandingPage />}/> 
+       
+        <Route path="/signin" element={<SignIn />}  />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/developers" element = {<Developers />} />
+      </Routes>
+      {/* <LandingPage /> */}
     </>
   )
 }
--- src/components/landingPage/Navbar.jsx
+++ src/components/landingPage/Navbar.jsx
@@ -1,18 +1,19 @@
 import React from 'react'
+import { Link, NavLink } from 'react-router-dom'
 
 const Navbar = () => {
   return (
     <div className='container mx-auto px-10 py-6 flex items-center justify-between font-primaryFont'>
 
             <div className='text-xl font-semibold'>
-                  Finance <span className='text-primary'>Paddy</span>
+                 <NavLink to={'/'}>Finance <span className='text-primary'>Paddy</span></NavLink>
              </div>
 
 
             <div className='flex gap-4 items-center'>
-              <p className='font-semibold text-appGrey cursor-pointer'>Developers</p>
-              <button className='border-[1.5px] border-primary py-2.5 px-6 rounded-lg text-primary font-semibold '>SignIn</button>
-              <button className='py-2.5 px-6 rounded-lg text-white font-semibold bg-primary'>Signup</button>
+              <NavLink to = {'/developers'} className='font-semibold text-appGrey cursor-pointer'>Developers</NavLink> 
+              <NavLink to={'/signin'} className='border-[1.5px] border-primary py-2.5 px-6 rounded-lg text-primary font-semibold '>SignIn</NavLink>
+              <NavLink to={'/signup'} className='py-2.5 px-6 rounded-lg text-white font-semibold bg-primary'>Signup</NavLink>
             </div>
 
     </div>
