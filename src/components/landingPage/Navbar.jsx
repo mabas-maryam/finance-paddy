89b4f980d3da5834abbe47d3c3fcdcd4ca043c2d
unknown <mabas.maryam>
2024-07-02 15:39:45 +0100

    responsivness

--- src/components/landingPage/Navbar.jsx
+++ src/components/landingPage/Navbar.jsx
@@ -3,14 +3,14 @@ import { Link, NavLink } from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <div className='container mx-auto px-10 py-6 flex items-center justify-between font-primaryFont'>
+    <div className='container mx-auto px-4 py-6 flex items-center justify-between font-primaryFont md:px-10'>
 
             <div className='text-xl font-semibold'>
                  <NavLink to={'/'}>Finance <span className='text-primary'>Paddy</span></NavLink>
              </div>
 
 
-            <div className='flex gap-4 items-center'>
+            <div className='hidden gap-4 items-center md:flex'>
               <NavLink to = {'/developers'} className='font-semibold text-appGrey cursor-pointer'>Developers</NavLink> 
               <NavLink to={'/signin'} className='border-[1.5px] border-primary py-2.5 px-6 rounded-lg text-primary font-semibold '>SignIn</NavLink>
               <NavLink to={'/signup'} className='py-2.5 px-6 rounded-lg text-white font-semibold bg-primary'>Signup</NavLink>
